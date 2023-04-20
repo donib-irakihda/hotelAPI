90138a15d1c10b44325c048b1c9330707eae521c
donib-irakihda <donib.irakihda@gmail.com>
2023-04-20 12:00:06 +0545

    fix : quick fixes

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,12 +1,25 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const db = mongoose.connect(process.env.MONGO_URI, {
+let db
+if(process.env.NODE_ENV !== 'test'){
+    db = mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(async ()=>{
-        console.log("Database Connected");
+        console.log("Production Database Connected");
     }).catch(err=>{
         console.log(err);
         console.log("Error connecting database!!!.");
     });
+} else {
+        db = mongoose.connect(process.env.MONGO_TEST_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }).then(async ()=>{
+                console.log("Test Database Connected");
+            }).catch(err=>{
+                console.log(err);
+                console.log("Error connecting database!!!.");
+            });
+}
 module.exports = db;
