24bbd4d8998d13e1f1d870ef2fe05bc49329ba0e
donib-irakihda <donib.irakihda@gmail.com>
2023-04-20 11:12:51 +0545

    fixed auth middleware

--- test/user.test.js
+++ test/user.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest')
 const {app} = require('../src/index')
+// const { MongoClient } = require('mongodb');
+
 
 describe("test for user", () => {
     it('register a user', async () => {
@@ -7,12 +9,12 @@ describe("test for user", () => {
         .post('/api/users/register')
         .send({
             "name": "Donib",
-            "email": "a21@abc1.com",
+            "email": "catt@abc1.com",
             "password": "hacker"
         });
 
         console.log("response",response.body)
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(201);
         
         expect(response.body.message).toBe("User registration success");
 
@@ -23,12 +25,26 @@ describe("test for user", () => {
         const response = await request(app)
         .post('/api/users/login')
         .send({
-            "email": "abekkf@abc1.com",
-            "password": "hackerr"
+            "email": "cow1@abc1.com",
+            "password": "hacker"
         });
         console.log("GG", response.body)
         // expect(response.statusCode).toBe(400);
-        expect(response.body.message).toBe("Invalid credentials")
+        expect(response.body.message).toBe("Login success")
     },600000)
         
 })
+
+// afterAll(async () => {
+//     const client = new MongoClient("mongodb+srv://donib:code@cluster0.ihj6xk2.mongodb.net/?retryWrites=true&w=majority");
+//     await client.connect()
+//     const db = client.db()
+//     const dbs = await db.admin().listDatabases()
+//     for (let data of dbs.databases) {
+//         if (data.name.toString().includes("production")) {
+//             var dbo = client.db(data.name)
+//             dbo.dropDatabase()
+//         }
+//     }
+//     // client.close()
+// })
